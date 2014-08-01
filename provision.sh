5b9b48babe7d2136fa2f3b713c9368bf679e94bc
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2014-08-01 19:09:22 -0400

    Added necessary clang libraries

--- provision.sh
+++ provision.sh
@@ -7,4 +7,4 @@ set -x # Print commands and their arguments as they are executed.
 sudo apt-get update -y
 
 # Install required Debian packages.
-sudo apt-get install -y git clang-3.5
+sudo apt-get install -y git clang-3.5 libclang-common-3.5-dev libclang1-3.5 libc++-dev libc++-helpers libc++1
