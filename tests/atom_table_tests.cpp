181baa4a56fefbb2cc45394b257c8e88c9915855
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2014-08-03 20:45:01 +0000

    Atom tables use slices instead of strings for their keys. This implies that all keys stored in an atom_table will outlive the atom_table,
    Added atom_table unit tests.

---
+++ tests/atom_table_tests.cpp
@@ -0,0 +1,57 @@
+#include "catch.hpp"
+#include "tools.hpp"
+
+using pdf::tools::slice;
+using pdf::tools::atom_type;
+using pdf::tools::atom_table;
+
+TEST_CASE("atom_table: add", "[atom_table]") {
+    atom_table t;
+
+    CHECK(t.add("xyzzy") >= 0);
+    CHECK(t.add("plugh") >= 0);
+    CHECK(t.add("plover") >= 0);
+
+    CHECK(t["xyzzy"] == t["xyzzy"]);
+    CHECK(t["plugh"] == t["plugh"]);
+    CHECK(t["plover"] == t["plover"]);
+
+    CHECK(t["xyzzy"] != t["plugh"]);
+    CHECK(t["xyzzy"] != t["plover"]);
+
+    CHECK(t["plugh"] != t["xyzzy"]);
+    CHECK(t["plugh"] != t["plover"]);
+
+    CHECK(t["plover"] != t["xyzzy"]);
+    CHECK(t["plover"] != t["plugh"]);
+}
+
+TEST_CASE("atom_table: rebased", "[atom_table]") {
+    atom_table t(100);
+
+    CHECK(t["xyzzy"] == t["xyzzy"]);
+    CHECK(t["plugh"] == t["plugh"]);
+    CHECK(t["plover"] == t["plover"]);
+
+    CHECK(t["xyzzy"] != t["plugh"]);
+    CHECK(t["xyzzy"] != t["plover"]);
+
+    CHECK(t["plugh"] != t["xyzzy"]);
+    CHECK(t["plugh"] != t["plover"]);
+
+    CHECK(t["plover"] != t["xyzzy"]);
+    CHECK(t["plover"] != t["plugh"]);
+}
+
+TEST_CASE("atom_table: pre-defined", "[atom_table]") {
+    enum atom : atom_type { xyzzy, plugh, plover };
+
+    atom_table t;
+    t.add("xyzzy", atom::xyzzy);
+    t.add("plugh", atom::plugh);
+    t.add("plover", atom::plover);
+
+    CHECK(t["xyzzy"] == atom::xyzzy);
+    CHECK(t["plugh"] == atom::plugh);
+    CHECK(t["plover"] == atom::plover);
+}
