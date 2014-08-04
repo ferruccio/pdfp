b3822d0f6046a3001011b7a24b4b93412835e32f
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2014-08-04 11:31:13 +0000

    Locate PDF trailer.

--- src/pdfp.cpp
+++ src/pdfp.cpp
@@ -9,15 +9,20 @@ namespace pdf {
 
     class PdfParser : public IPdfParser {
     public:
-        PdfParser(slice pdf) : pdf(pdf) {
-            if (!pdf.starts_with("%PDF-1."))
-                throw pdf_error("no pdf header");
-        }
+        PdfParser(slice pdf) : pdf(pdf) { init(); }
 
         ~PdfParser() {}
 
     private:
         slice pdf;
+
+        void init() {
+            if (!pdf.starts_with("%PDF-1."))
+                throw pdf_error("no pdf header");
+            slice trailer = pdf.find_last("trailer");
+            if (trailer.empty())
+                throw pdf_error("no pdf trailer");
+        }
     };
 
     auto make_pdf_parser(const char* begin, const char* end) -> std::unique_ptr<IPdfParser> {
